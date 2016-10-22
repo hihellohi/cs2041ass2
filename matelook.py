e1b3af1097d216d452eb0f367074083f6f06ecb7
Kevin <hihellohi610@gmail.com>
2016-10-22 13:57:42 +1100

    login

--- matelook.py
+++ matelook.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3.5 
 import sqlite3;
-from flask import Flask, render_template, request, g;
+import os;
+from flask import Flask, session, redirect, render_template, request, g;
 
 app = Flask(__name__);
+app.secret_key = os.urandom(420);
 
 database = "dataset.db";
 
@@ -28,9 +30,33 @@ def close_connection(exception):
 		db.close();
 #endsrc
 
+def get_template(f, **args):
+	args['login'] = session.get('login', None);
+	return render_template(f, **args);
+
 @app.route('/')
-def hello_world():
-	return render_template("main.html", level='.');
+def root():
+	return get_template("main.html", level='.');
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+	if 'login' in session:
+		return redirect("..");
+	if request.method == 'GET':
+		return get_template("login.html", level='..');
+	else:
+		num = request.form['zid'].lstrip('z');
+		password = query_db("SELECT password FROM users WHERE zid = ?", [num], one=True);
+		if password and password['password'] == request.form['password']:
+			session['login'] = num;
+			return redirect("..");
+		else:
+			return get_template("login.html", level='..', zid=request.form['zid']);
+
+@app.route('/logoff/')
+def logoff():
+	session.pop('login', None);
+	return redirect('..');
 
 @app.route('/z<int:stuid>/')
 def profile_page(stuid):
@@ -43,23 +69,23 @@ def profile_page(stuid):
 	ORDER BY posts.date DESC, posts.time DESC""", [stuid]);
 
 	if not profile is None:
-		return render_template("profile.html", level="..", profile=profile, mates=mates, posts=posts);
-	return render_template("main.html");
+		return get_template("profile.html", level="..", profile=profile, mates=mates, posts=posts);
+	return redirect("..");
 
-@app.route('/search/')
+@app.route('/search/', methods=['GET'])
 def search():
 	if 'search' in request.args:
 		if request.args['criteria'] == 'users':
 			results = query_db("SELECT zid, name, dp FROM users WHERE name LIKE ?", ['%' + request.args['terms'] + '%']);
-			return render_template("suser.html", level="..", terms=request.args['terms'], users=results);
+			return get_template("suser.html", level="..", terms=request.args['terms'], users=results);
 		else:
 			results = query_db(
 			"""SELECT posts.zid, posts.message, posts.date, posts.time, users.name, users.dp 
 			FROM posts INNER JOIN users ON posts.zid = users.zid WHERE posts.message LIKE ? 
 			ORDER BY posts.date DESC, posts.time DESC""", ['%' + request.args['terms'] + '%']);
-			return render_template("spost.html", level="..", terms=request.args['terms'], posts=results);
+			return get_template("spost.html", level="..", terms=request.args['terms'], posts=results);
 	else:
-		return render_template("srequest.html", level="..");
+		return get_template("srequest.html", level="..");
 
 @app.route('/static/<path:path>')
 def send_static_file(path):
